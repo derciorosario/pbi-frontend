666483b3cca17499104b7f9bb630cd8803b74392
derciorosario <derciorosario55@gmail.com>
2025-09-10 15:37:23 +0200

    .

--- src/components/TopFilterButtons.jsx
+++ src/components/TopFilterButtons.jsx
@@ -5,8 +5,8 @@ export default function TopFilterButtons({buttons=[],selected=[],setSelected,fro
  const data=useData()
  const {user}=useAuth()
   return (
-     <div className="flex relative gap-2 w-full overflow-x-auto rounded-sm whitespace-nowrap py-2 _sticky z-20 _bg-[#F7F7FB]">
-              {buttons.map(i=>(
+     <div className="flex relative gap-y-2 w-full overflow-x-auto rounded-sm whitespace-nowrap py-2 _sticky z-20 _bg-[#F7F7FB]">
+              {buttons.map((i,_i)=>(
                 <button
                        onClick={() => {
                           if(selected.includes(i)){
@@ -26,7 +26,7 @@ export default function TopFilterButtons({buttons=[],selected=[],setSelected,fro
                           }
                           data.setUpdateData(Math.random())
                        }}
-                       className={`rounded-full table px-2.5 py-1.5 text-sm font-medium ${!selected.includes(i) ? '  text-gray-700 hover:border-brand-300 ':'text-brand-600 bg-brand-50 active:bg-brand-800'} shadow-sm  flex items-center justify-center gap-2 transition-all duration-200 hover:shadow-md flex-shrink-0`}
+                       className={`table px-3 py-1.5  text-sm font-medium ${!selected.includes(i) ? '  text-gray-700 hover:border-brand-300 ':'text-brand-600 font-bold  active:bg-brand-800'} ${_i === 0 ? 'border-r' : _i === buttons.length - 1 ? 'border-l' : 'border-x'} border-gray-200 flex items-center justify-center gap-2 transition-all duration-200 hover:shadow-md flex-shrink-0`}
                      >
                        <span>{i}</span>
                      </button>
